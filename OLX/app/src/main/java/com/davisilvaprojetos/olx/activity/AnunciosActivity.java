a9befb3c98b18019e69846a1dd18c879b32ebb06
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-02-17 10:44:51 -0300

    -Criando interface de anúncios públicos e listando. -Aplicando filtros por estado

--- OLX/app/src/main/java/com/davisilvaprojetos/olx/activity/AnunciosActivity.java
+++ OLX/app/src/main/java/com/davisilvaprojetos/olx/activity/AnunciosActivity.java
@@ -2,24 +2,167 @@ package com.davisilvaprojetos.olx.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.davisilvaprojetos.olx.R;
+import com.davisilvaprojetos.olx.adapter.AdapterAnuncios;
 import com.davisilvaprojetos.olx.helper.ConfiguracaoFirebase;
+import com.davisilvaprojetos.olx.model.Anuncio;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
 
 public class AnunciosActivity extends AppCompatActivity {
     private FirebaseAuth autenticacao ;
+    private RecyclerView recyclerAnunciosPublicos;
+    private Button buttonRegiao, buttonCategoria;
+    private AdapterAnuncios adapterAnuncios;
+    private List<Anuncio> listaAnuncios = new ArrayList<>();
+    private DatabaseReference anunciosPublicosRef;
+    private AlertDialog dialog;
+    private String filtroEstado = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anuncios);
+        inicializarComponentes();
         //Configurações iniciais
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        anunciosPublicosRef = ConfiguracaoFirebase.getFirebase()
+                .child("anuncios");
+
+        //Configurar RecyclerView
+        recyclerAnunciosPublicos.setLayoutManager(new LinearLayoutManager(this));
+        recyclerAnunciosPublicos.setHasFixedSize(true);
+        adapterAnuncios = new AdapterAnuncios(listaAnuncios,this);
+        recyclerAnunciosPublicos.setAdapter(adapterAnuncios);
+
+        recuperarAnunciosPublicos();
+    }
+
+    public void filtrarPorEstado(View view){
+        AlertDialog.Builder dialogEstado = new AlertDialog.Builder(this);
+        dialogEstado.setTitle("Selecione o estado desejado");
+        //Configurar o spinner
+        View viewSpinner = getLayoutInflater().inflate(R.layout.dialog_spinner,null);
+        //Configura spinner de estados
+        Spinner spinnerEstado = viewSpinner.findViewById(R.id.spinnerFiltro);
+        String[] estados = getResources().getStringArray(R.array.estados);
+        ArrayAdapter<String> adapterEstados = new ArrayAdapter<>(
+                this,   android.R.layout.simple_spinner_item,
+                estados
+        );
+        adapterEstados.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerEstado.setAdapter(adapterEstados);
+
+        dialogEstado.setView(viewSpinner);
+
+        dialogEstado.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                    filtroEstado = spinnerEstado.getSelectedItem().toString();
+                    recuperarAnunciosPorEstado();
+            }
+        });
+
+        dialogEstado.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog dialog = dialogEstado.create();
+        dialog.show();
+    }
+
+    public void recuperarAnunciosPorEstado(){
+        dialog = new SpotsDialog.Builder()
+                .setContext(this)
+                .setMessage("Recuperando Anúncios")
+                .setCancelable(false)
+                .build();
+
+        dialog.show();
+        //Configura nó por estado
+        anunciosPublicosRef = ConfiguracaoFirebase.getFirebase()
+                .child("anuncios")
+                .child(filtroEstado);
+
+        anunciosPublicosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listaAnuncios.clear();
+                for(DataSnapshot categorias: snapshot.getChildren()){
+                    for (DataSnapshot anuncios: categorias.getChildren()){
+                        Anuncio anuncio = anuncios.getValue(Anuncio.class);
+                        listaAnuncios.add(anuncio);
+                    }
+                }
+
+                Collections.reverse(listaAnuncios);
+                adapterAnuncios.notifyDataSetChanged();
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void recuperarAnunciosPublicos(){
+        dialog = new SpotsDialog.Builder()
+                .setContext(this)
+                .setMessage("Recuperando Anúncios")
+                .setCancelable(false)
+                .build();
+
+        dialog.show();
+        listaAnuncios.clear();
+        anunciosPublicosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot estados: snapshot.getChildren()){
+                    for(DataSnapshot categorias: estados.getChildren()){
+                        for (DataSnapshot anuncios: categorias.getChildren()){
+                            Anuncio anuncio = anuncios.getValue(Anuncio.class);
+                            listaAnuncios.add(anuncio);
+                        }
+                    }
+                }
+                Collections.reverse(listaAnuncios);
+                adapterAnuncios.notifyDataSetChanged();
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
@@ -54,5 +197,8 @@ public class AnunciosActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+    public void inicializarComponentes(){
+        recyclerAnunciosPublicos = findViewById(R.id.recyclerAnunciosPublicos);
     }
 }
